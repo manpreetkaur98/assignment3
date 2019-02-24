37202e28319fc67f6fe5447c46ae55c0bef1edde
manpreetkaur98 <43333432+manpreetkaur98@users.noreply.github.com>
2019-02-25 04:42:53 +1100

    Add files via upload

---
+++ assignment 3/ConsoleApp1(coffee problem solution)/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1_coffee_problem_solution_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string AgainChoice = "";
+            int TotalCoffeeCost = 0;,,,,,,,,,,,,,,,,,
+            do
+            {
+                
+                Console.WriteLine("1-small, 2-Medium, 3-Large");
+                int UserChoice = int.Parse(Console.ReadLine());
+
+                switch (UserChoice)
+                {
+                    case 1:
+                        TotalCoffeeCost += 1;
+                        break;
+                    case 2:
+                        TotalCoffeeCost += 2;
+                        break;
+                    case 3:
+                        TotalCoffeeCost += 3;
+                        break;
+                    default:
+                        Console.WriteLine("Your Choice {0} is invalid", UserChoice);
+                        break;
+
+
+                }
+                
+
+                do
+                {
+                    Console.WriteLine("Do you Want to buy another coffee");
+                    AgainChoice = Console.ReadLine();
+                    if (AgainChoice != "yes" && AgainChoice != "no")
+                    {
+                        Console.WriteLine("Invalide Choice");
+                    }
+                } while (AgainChoice != "yes" && AgainChoice != "no");
+
+            } while (AgainChoice == "yes");
+            Console.WriteLine("ThankYou");
+            Console.WriteLine("Bill Amount={0}", TotalCoffeeCost);
+        }
+    }
+}
